5f80119fdf0e0370a053a8c02ab955090ddc66f2
Martin Plascencia <luis@emrock.net>
2025-03-31 20:47:21 -0400

    Update main export for Atom

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+export * from './Atom';
 export * from './Container';
 export * from './hooks';
 export * from './Injectable';
 export * from './Store';
 export * from './types';
-export * from './Value';
